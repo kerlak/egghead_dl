5a517852ada2ba8c7d70957ab962c97cccd9841f
Juan Carlos Pazos Mandiá <jucapaman@gmail.com>
2019-05-28 16:03:12 +0200

    Restore :ok atom, to detect fails on video downloads

--- lib/video_downloader.ex
+++ lib/video_downloader.ex
@@ -3,7 +3,7 @@ use FFmpex.Options
 
 defmodule VideoDownloader do
    def download(m3u8_url, output_path) do
-    _ = FFmpex.new_command
+    :ok = FFmpex.new_command
             |> add_global_option(option_y())
             |> add_input_file(m3u8_url)
             |> add_output_file(output_path)
